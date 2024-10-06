f8db3d72cf8162ca867c688b61407d2d7ea46d2d
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-06 18:00:14 +0530

    Refactor: Update authentication method in Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/utils/authOptions";
+
 import Logout from "./Logout";
+import { auth } from "@/utils/auth";
 
 const Header = async () => {
-  const session = await getServerSession(authOptions);
+  const session = await auth();
+  console.log(session);
 
   return (
     <header className="w-full h-[80px] bg-gray-300 flex items-center justify-between px-10">
