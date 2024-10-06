a9adfdd0d925011db58ac2f9be05c19d13895d0f
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-06 16:25:53 +0530

    feat: Add createProject function for creating a new project

--- src/server/create-project.js
+++ src/server/create-project.js
@@ -1,11 +1,38 @@
 import { pinata } from "@/utils/config";
 import React from "react";
 
-export const createProject = async () => {
+export const createProject = async ({ project, isPublic }) => {
   const group = await pinata.groups.create({
-    name: "My Pa Group",
-    isPublic: true,
+    name: project,
+    // isPublic: isPublic,
   });
 
   console.log("Group created", group);
+
+  const projectData = {
+    name: project,
+    cid: group.id,
+    size: 0,
+    number_of_files: 0,
+    mime_type: "application/json",
+    group_id: group.id,
+    keyvalues: {},
+  };
+
+  const response = await fetch("/api/projects", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(projectData),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(`Failed to create project: ${errorData.error}`);
+  }
+
+  const newProject = await response.json();
+  console.log("Project saved to database", newProject);
+  return newProject;
 };
