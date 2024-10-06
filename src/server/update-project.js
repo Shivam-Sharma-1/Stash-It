b70b37e21b2cb8ea0310cf809471986b5f80d8bd
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-06 19:33:06 +0530

    Add updateProject function to handle project updates

---
+++ src/server/update-project.js
@@ -0,0 +1,36 @@
+import { pinata } from "@/utils/config";
+import React from "react";
+
+export const updateProject = async ({ groupId, project }) => {
+  const group = await pinata.groups.update({
+    groupId: groupId,
+    name: project,
+  });
+
+  console.log("Group updated", group);
+
+  const projectData = {
+    name: project,
+    groupId: group.id,
+    createdAt: group.createdAt,
+    updatedAt: group.updatedAt,
+    pinataUserId: group.user_id,
+  };
+
+  const response = await fetch(`/api/projects/${groupId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(projectData),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(`Failed to create project: ${errorData.error}`);
+  }
+
+  const newProject = await response.json();
+  console.log("Project saved to database", newProject);
+  return newProject;
+};
