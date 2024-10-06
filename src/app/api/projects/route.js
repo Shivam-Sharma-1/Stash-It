c24d04efde13a58b6b6abbe1f83f8c69817a983c
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-06 18:00:01 +0530

    Refactor: Update project creation route to use new authentication method

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -1,36 +1,47 @@
-import { authOptions } from "@/utils/authOptions";
-import { getServerSession } from "next-auth";
 import { prisma } from "../../../../prisma/prisma";
+import { auth } from "@/utils/auth";
 
-export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
+export async function POST(req) {
+  const session = await auth();
 
-  if (req.method === "POST") {
-    if (!session) return res.status(401).json({ error: "Unauthorized" });
+  if (!session) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+    });
+  }
 
-    const { name, cid, size, number_of_files, mime_type, group_id, keyvalues } =
-      req.body;
+  const { name, groupId, createdAt, updatedAt, pinataUserId } =
+    await req.json();
 
-    try {
-      const newProject = await prisma.project.create({
-        data: {
-          name,
-          cid,
-          size,
-          number_of_files,
-          mime_type,
-          group_id,
-          keyvalues,
-          userId: session.user.id,
+  console.log("Received request:", req.method);
+  console.log("Request body:", {
+    name,
+    groupId,
+    createdAt,
+    updatedAt,
+    pinataUserId,
+  });
+
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        groupId,
+        createdAt,
+        updatedAt,
+        pinataUserId,
+        user: {
+          connect: {
+            id: session.user.id,
+          },
         },
-      });
-      res.status(201).json(newProject);
-    } catch (error) {
-      console.error("Error creating project:", error);
-      res.status(500).json({ error: "Failed to create project" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+      },
+    });
+    return new Response(JSON.stringify(newProject), { status: 201 });
+  } catch (error) {
+    console.error("Error creating project:", error);
+    return new Response(JSON.stringify({ error: "Failed to create project" }), {
+      status: 500,
+    });
   }
 }
