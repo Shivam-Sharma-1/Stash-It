9de86f9ba09384bc295f1f6094b82df059c8cedb
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-11 15:26:33 +0530

    Refactor: Update environment variables in constants/index.js

--- src/lib/constants/index.js
+++ src/lib/constants/index.js
@@ -1,1 +1,8 @@
 export const BASE_URL = process.env.BASE_URL;
+export const PINATA_GATEWAY_URL = process.env.PINATA_GATEWAY_URL;
+export const PINATA_JWT = process.env.PINATA_JWT;
+
+export const GITHUB_ID = process.env.GITHUB_ID;
+export const GITHUB_SECRET = process.env.GITHUB_SECRET;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
