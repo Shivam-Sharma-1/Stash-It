b81baa23b1bcae117514c1fdc1dfb9b3ffaf6300
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-11 14:21:38 +0530

    Refactor: Update checkUser.js to use BASE_URL constant for redirect

--- src/lib/checkUser.js
+++ src/lib/checkUser.js
@@ -2,12 +2,13 @@
 
 import { auth } from "@/utils/auth";
 import { redirect } from "next/navigation";
+import { BASE_URL } from "./constants";
 
 export const checkUser = async () => {
   const session = await auth();
   if (!session) {
     console.log("Unauthorized access");
-    return redirect("/auth");
+    return redirect(`${BASE_URL}/auth`);
   }
 
   return session;
