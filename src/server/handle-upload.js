a681f6d93b84ab3103082fa1f0e82e4035e5aa21
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-11 15:19:19 +0530

    Refactor: Update handle-upload.js to handle file uploads, validate formData, and handle errors

--- src/server/handle-upload.js
+++ src/server/handle-upload.js
@@ -3,9 +3,23 @@
 import { checkUser } from "@/lib/checkUser";
 import { pinata } from "@/utils/config";
 
-export const handleUpload = async (file, groupId) => {
+export const handleUpload = async (formData) => {
   await checkUser();
 
-  const upload = await pinata.upload.file(file).group(groupId);
-  console.log("uploaded", upload);
+  const file = formData.get("file");
+  const groupId = formData.get("groupId");
+
+  if (!file || !groupId) {
+    throw new Error("File or groupId is missing");
+  }
+
+  try {
+    const upload = await pinata.upload.file(file).group(groupId);
+    console.log("uploaded", upload);
+
+    return { success: true, data: upload };
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw new Error("Failed to upload file");
+  }
 };
