b9083c77988bf51a3dfeeef4ee496d7620179eb5
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2024-10-12 22:24:42 +0530

    feat: Add invalidation for DeleteAsset

--- src/components/Project/AssetActions.jsx
+++ src/components/Project/AssetActions.jsx
@@ -15,12 +15,30 @@ import { PiTrash } from 'react-icons/pi';
 import { deleteAsset } from '@/server/delete-asset';
 import UpdateAsset from './UpdateAsset';
 import ShareAsset from './ShareAsset';
-
-const handleDelete = async (cid) => {
-  const res = await deleteAsset({ cid });
-};
+import { useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { toast } from 'sonner';
 
 const AssetActions = ({ cid, asset }) => {
+  const queryClient = useQueryClient();
+  const { groupId } = useParams();
+  const handleDelete = async (cid) => {
+    const res = await deleteAsset({ cid });
+    if (res) {
+      toast.success('Deleted Asset');
+      queryClient.invalidateQueries({
+        queryKey: [
+          'assets',
+          {
+            groupId: groupId,
+          },
+        ],
+        refetchType: 'all',
+      });
+    } else {
+      toast.error('Failed to delete asset.');
+    }
+  };
   return (
     <DropdownMenu dialog={false}>
       <DropdownMenuTrigger>
