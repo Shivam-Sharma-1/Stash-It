8c4d29b24eae54bf163152f02ae153ff4cfa1419
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-14 01:42:49 +0530

    refactor: Refactor UpdateAsset component

--- src/components/Project/UpdateAsset.jsx
+++ src/components/Project/UpdateAsset.jsx
@@ -1,22 +1,21 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import UpdateAssetForm from './UpdateAssetForm';
-import { PencilSimple } from '@phosphor-icons/react/';
+} from "@/components/ui/dialog";
+import { PencilSimple } from "@phosphor-icons/react/";
+import UpdateAssetForm from "./UpdateAssetForm";
 
 const UpdateAsset = ({ cid, asset }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-      <DialogTrigger className='w-full text-sm flex flex-row items-center justify-start hover:bg-secondary px-2 py-1.5'>
-        <PencilSimple className='mr-2 h-4 w-4' />
+      <DialogTrigger className="w-full text-sm flex flex-row items-center justify-start hover:bg-secondary px-2 py-1.5">
+        <PencilSimple className="mr-2 h-4 w-4" />
         Edit
       </DialogTrigger>
       <DialogContent>
