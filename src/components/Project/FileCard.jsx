67fda9304a80ec07cd1e6f85abec6f2fb9da7aba
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-14 00:34:40 +0530

    refactor: Refactor FileCard component

--- src/components/Project/FileCard.jsx
+++ src/components/Project/FileCard.jsx
@@ -1,55 +1,59 @@
-'use client';
-import React from 'react';
-import { Card, CardContent } from '../ui/card';
-import Image from 'next/image';
-import { File, FileAudio, Video } from '@phosphor-icons/react';
-import AssetActions from './AssetActions';
+"use client";
+import React from "react";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+import { File, FileArchive, FileAudio, Video } from "@phosphor-icons/react";
+import AssetActions from "./AssetActions";
 
 export default function FileCard({ fileData, isExplore }) {
   const type = fileData.mime_type;
   console.log(fileData);
   return (
-    <Card className='hover:bg-secondary hover:cursor-pointer'>
-      <CardContent className='p-4'>
-        <div className='flex w-full aspect-square flex-col items-center justify-center p-2'>
-          <div className='w-full h-full flex items-center justify-center'>
-            {type.startsWith('image/') ? (
+    <Card className="hover:bg-secondary hover:cursor-pointer">
+      <CardContent className="p-4">
+        <div className="flex w-full aspect-square flex-col items-center justify-center p-2">
+          <div className="w-full h-full flex items-center justify-center">
+            {type.startsWith("image/") ? (
               <Image
                 priority
-                data-fancybox='gallery'
-                className='cursor-pointer'
-                src={fileData.url ?? ''}
-                alt='file'
+                data-fancybox="gallery"
+                className="cursor-pointer"
+                src={fileData.url ?? ""}
+                alt="file"
                 width={350}
                 height={350}
               />
-            ) : type.startsWith('video/') || type.startsWith('audio/') ? (
+            ) : type.startsWith("video/") || type.startsWith("audio/") ? (
               <a
                 data-fancybox
-                data-type='html5video'
+                data-type="html5video"
                 controls
                 href={fileData.url}
-                className='w-full h-full items-center justify-center flex'
+                className="w-full h-full items-center justify-center flex"
               >
-                {type.startsWith('video/') && (
+                {type.startsWith("video/") && (
                   <Video
                     size={80}
-                    className='min-w-full'
-                    color='#ff4444'
-                    weight='duotone'
+                    className="min-w-full"
+                    color="#ff4444"
+                    weight="duotone"
                   />
                 )}
-                {type.startsWith('audio/') && (
-                  <FileAudio size={80} color='#ff4444' weight='duotone' />
+                {type.startsWith("audio/") && (
+                  <FileAudio size={80} color="#ff4444" weight="duotone" />
                 )}
               </a>
+            ) : type.startsWith("application/") ? (
+              <a href={fileData.url} target="_blank" rel="noreferrer">
+                <File size={80} color="#ff4444" weight="duotone" />
+              </a>
             ) : (
-              <File size={80} color='#ff4444' weight='duotone' />
+              <FileArchive size={80} color="#ff4444" weight="duotone" />
             )}
           </div>
         </div>
-        <div className='flex flex-row justify-between items-center text-lg font-medium w-full'>
-          <span className='w-full truncate'>{fileData.metadata.name}</span>
+        <div className="flex flex-row justify-between items-center text-lg font-medium w-full">
+          <span className="w-full truncate">{fileData.metadata.name}</span>
           <AssetActions
             isExplore={isExplore}
             cid={fileData.ipfs_pin_hash}
