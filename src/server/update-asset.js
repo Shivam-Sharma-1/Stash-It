1e1e16bc648358499c38b343b2d2b1a816e58274
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-14 07:24:45 +0530

    refactor: Update handleUpload function to include name and description metadata

--- src/server/update-asset.js
+++ src/server/update-asset.js
@@ -1,21 +1,24 @@
-'use server';
+"use server";
 
-import { checkUser } from '@/lib/checkUser';
-import { pinata } from '@/utils/config';
+import { checkUser } from "@/lib/checkUser";
+import { pinata } from "@/utils/config";
 
-export const updateAsset = async ({ cid, asset }) => {
+export const updateAsset = async ({ cid, name, description }) => {
   await checkUser();
 
   try {
     const updatedAsset = await pinata.updateMetadata({
       cid: cid,
-      name: asset,
+      name: name,
+      keyValues: {
+        description: description ? description : "",
+      },
     });
 
-    console.log('Updated file:', updatedAsset);
+    console.log("Updated file:", updatedAsset);
     return { updatedAsset };
   } catch (error) {
-    console.error('Error updating file:', error);
-    throw new Error('Failed to update file:', error);
+    console.error("Error updating file:", error);
+    throw new Error("Failed to update file:", error);
   }
 };
