20a254b1f97aa6cea859a894c508c99873cfd23a
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-14 11:32:28 +0530

    refactor: Update handle-multifile-upload.js

--- src/server/handle-multifile-upload.js
+++ src/server/handle-multifile-upload.js
@@ -1,26 +1,29 @@
-'use server';
+"use server";
 
-import { checkUser } from '@/lib/checkUser';
-import { pinata } from '@/utils/config';
+import { checkUser } from "@/lib/checkUser";
+import { pinata } from "@/utils/config";
 
 export const handleMultiUpload = async (formData) => {
   await checkUser();
 
-  const groupId = formData.get('groupId');
+  const groupId = formData.get("groupId");
 
   if (!groupId) {
-    throw new Error('GroupId is missing');
+    throw new Error("GroupId is missing");
   }
 
   let successCount = 0;
   const errors = [];
 
   for (const [key, value] of formData.entries()) {
-    if (key.startsWith('file') && value instanceof File) {
+    if (key.startsWith("file") && value instanceof File) {
       try {
         const upload = await pinata.upload.file(value).group(groupId);
-        console.log('uploaded', upload);
+        console.log("uploaded", upload);
         successCount++;
+        if (!upload) {
+          errors.push(`Failed to upload ${value.name}`);
+        }
       } catch (error) {
         console.error(`Error uploading file ${key}:`, error);
         errors.push(`Failed to upload ${value.name}`);
@@ -29,7 +32,7 @@ export const handleMultiUpload = async (formData) => {
   }
 
   if (errors.length > 0) {
-    console.error('Errors during upload:', errors);
+    console.error("Errors during upload:", errors);
   }
 
   return {
