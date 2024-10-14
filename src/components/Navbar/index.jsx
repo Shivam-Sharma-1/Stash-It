e022b5b13ea76430a880a1c44b5963e403fa6c45
Shivam77607 <shivamsharma77607@gmail.com>
2024-10-14 07:52:11 +0530

    refactor: Update Navbar component to include login button when user is not logged in

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import Logout from './Logout';
-import { auth } from '@/utils/auth';
-import Logo from '../LandingPage/Logo';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import Link from 'next/link';
-import { Button } from '../ui/button';
+import Logout from "./Logout";
+import { auth } from "@/utils/auth";
+import Logo from "../LandingPage/Logo";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Navbar = async () => {
   const session = await auth();
   return (
-    <header className='w-full h-20 bg-background border-b border-border flex items-center justify-between px-10'>
-      <Link href='/'>
+    <header className="w-full h-20 bg-background border-b border-border flex items-center justify-between px-10">
+      <Link href="/">
         <Logo />
       </Link>
-      <div className='flex flex-row items-center gap-4'>
-        <Link className='hover:text-primary mr-4' href='/explore'>
+      <div className="flex flex-row items-center gap-4">
+        <Link className="hover:text-primary mr-4" href="/explore">
           Explore Assets
         </Link>
-        {session && (
+        {session ? (
           <>
-            <Button variant='outline' asChild>
-              <Link href={'/dashboard'}>Dashboard</Link>
+            <Button variant="outline" asChild>
+              <Link href={"/dashboard"}>Dashboard</Link>
             </Button>
             <Avatar>
-              <AvatarImage src={session.user.image ?? ''} />
+              <AvatarImage src={session.user.image ?? ""} />
               <AvatarFallback>
                 {session.user.email.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
             <Logout />
           </>
+        ) : (
+          <Button asChild>
+            <Link href={"/auth"}>Log In</Link>
+          </Button>
         )}
       </div>
     </header>
